f5a94fd7cc1803b623df1a0dd2084b2379696040
vivCoding <vivCoding@gmail.com>
2022-02-05 17:48:32 -0500

    barebones

--- backend/database/connect.py
+++ backend/database/connect.py
@@ -1,1 +1,12 @@
-# TODO: write connection code
+from pymongo import MongoClient
+from os import getenv
+
+class Connection:
+
+    client: MongoClient = None
+
+    # Initializes MongoDb client (from env), and sets static variable to client
+    @staticmethod
+    def init() -> bool:
+        # TODO: implement
+        pass
---
+++ backend/database/user.py
@@ -0,0 +1,22 @@
+from .connect import Connection
+
+class User:
+    collection = "users"
+
+    def __init__(self) -> None:
+        # TODO: initialize values from parameters
+        # self.username = "bob"
+        pass
+
+    # Pushes this object to MongoDB, and returns whether it was successful
+    def push(self) -> bool:
+        # TODO: implement
+        # Connection.client
+        pass
+
+    # Static method that finds and returns a specific user from the collection
+    @classmethod
+    def find(cls):
+        # TODO: implement
+        return User()
+
