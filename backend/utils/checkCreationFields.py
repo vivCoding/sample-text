4c0159f2fe800e5fafb2cc27f969e12bce210a24
Devin Qu <devinqu02@gmail.com>
2022-02-07 14:38:28 -0500

    add restriction on username length

--- backend/utils/checkCreationFields.py
+++ backend/utils/checkCreationFields.py
@@ -2,14 +2,16 @@ from validate_email import validate_email
 import re
 
 def checkCreationFields(username, email, password) -> int:
-    if username == "":  # empty username
-        return 1
+    if len(username) < 1 or len(username) > 20:
+        return 1        # username length must be in [1, 20]
+    if not re.match(r"""[a-zA-Z0-9\-_]{1,20}""", username):
+        return 2        # username contains invalid characters
     # check if username is in db here
     if not validate_email(email):
-        return 3        # email not valid
+        return 4        # email not valid
     # check if email is in db here
     if len(password) < 8 or len(password) > 25:
-        return 5        # password length must be in [8, 25]
+        return 6        # password length must be in [8, 25]
     if not re.match(r"""[a-zA-Z0-9~`!@#$%^&*()_\-+={[}\]|\:;'"<,>.?\/]{8,25}""", password):
-        return 6        # password contains invalid characters
+        return 7        # password contains invalid characters
 
