05637027951cdec7d932ce084c6c3448eca2bfb6
Devin Qu <devinqu02@gmail.com>
2022-02-07 21:13:55 -0500

    move database lookups to the end

--- backend/utils/checkCreationFields.py
+++ backend/utils/checkCreationFields.py
@@ -6,13 +6,13 @@ def checkCreationFields(username, email, password) -> int:
         return 1        # username length must be in [1, 20]
     if not re.match(r"""[a-zA-Z0-9\-_.]{1,20}""", username):
         return 2        # username contains invalid characters
-    # check if username is in db here
     if not validate_email(email):
-        return 4        # email not valid
-    # check if email is in db here
+        return 3        # email not valid
     if len(password) < 8 or len(password) > 25:
-        return 6        # password length must be in [8, 25]
+        return 4        # password length must be in [8, 25]
     if not re.match(r"""[a-zA-Z0-9~`!@#$%^&*()_\-+={[}\]|\:;'"<,>.?\/]{8,25}""", password):
-        return 7        # password contains invalid characters
+        return 5        # password contains invalid characters
+    # check if username is in db here
+    # check if email is in db here
     return 0            # fields are all valid
 
