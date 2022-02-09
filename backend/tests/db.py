c09d857222c05e6c4e1004e62df6c86c7e978a27
eyang2020 <kenzox2234@gmail.com>
2022-02-09 18:04:45 -0500

    added fail msg for tests

--- backend/tests/db.py
+++ backend/tests/db.py
@@ -7,14 +7,13 @@ user1 = User(username="bob123", email="bob123@gmail.com", password="bobPassword"
 user2 = User(username="alice456", email="alice456@purdue.edu", password="alicePassword")
 
 def test_push_user():
-    assert User.push(user1)
-    assert User.push(user2)
+    assert User.push(user1), 'Push on user1 failed'
+    assert User.push(user2), 'Push on user2 failed'
 
 def test_find_user_by_username():
-    assert User.find_by_username("bob123") == user1
-    assert User.find_by_username("randomUsername") is NULL
+    assert User.find_by_username("bob123") == user1, 'Could not find user1 by username'
+    assert User.find_by_username("randomUsername") is NULL, 'Found nonexistent user by username'
 
 def test_find_user_by_email():
-    assert User.find_by_email("alice456@purdue.edu") == user2
-    assert User.find_by_email("random@gmail.com") is NULL
-
+    assert User.find_by_email("alice456@purdue.edu") == user2, 'Could not find user2 by email'
+    assert User.find_by_email("random@gmail.com") is NULL, 'Found nonexistent user by email'
