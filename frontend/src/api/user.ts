7b8c71c577b3031305ddd33ac9d1457109e0586a
vivCoding <vivCoding@gmail.com>
2022-02-10 13:20:39 -0500

    frontend api status feedback

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,21 +1,43 @@
-import ResponseType from '../types/response'
+import ResponseType from '../types/api'
+import client from './httpClient'
 
 export const createUser = async (username: string, email: string, password: string): Promise<ResponseType> => {
-    const response = await fetch('localhost:5000/api/user/createaccount', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, email, password }),
-    })
-    if (response.status !== 200) {
-        return { success: false, error: -1 }
+    const response = await client.post('/user/createaccount', { username, email, password })
+    const error = {
+        email: '', username: '', password: '', server: '',
     }
-    const data = await response.json()
-    return data as ResponseType
+    if (response.status !== 500) {
+        error.server = 'There was an error signing you up. Try again later!'
+        return { success: false, data: error, error: -1 }
+    }
+    const data = response.data as ResponseType
+    switch (data.error) {
+        case 1:
+            error.username = 'Username length must contain 1 to 20 characters!'
+            break
+        case 2:
+            error.username = 'Username must be only contain letters, numbers, periods, dashes, and underscores!'
+            break
+        case 3:
+            error.email = 'Invalid email!'
+            break
+        case 4:
+            error.password = 'Password length must contain 8 to 25 characters!'
+            break
+        case 5:
+            error.password = 'Password length contains invalid characters!'
+            break
+        case 6:
+            error.username = 'Username already exists!'
+            break
+        case 7:
+            error.email = 'Email already exists!'
+            break
+        default: error.server = 'There was an error signing you up. Try again later!'
+    }
+    return { ...data, data: error }
 }
 
-export const loginUser = async (): Promise<any> => {
-    // TODO: implement and change return type
+export const loginUser = async (username: string, password: string): Promise<any> => {
+    // TODO: implement and add return type
 }
