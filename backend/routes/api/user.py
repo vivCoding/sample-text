2c429cdbc8ac39911f9d76ba103b01d0578471a4
Ryan Chu <plochemisswag@gmail.com>
2022-02-10 04:54:41 -0500

    hash pwd w/ md5

--- backend/routes/api/user.py
+++ backend/routes/api/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from utils.checkCreationFields import checkCreationFields
 from database.user import User
+import hashlib
 
 user_blueprint = Blueprint("user", __name__)
 
@@ -15,7 +16,8 @@ def create_account():
 	try:
 		if status == 0:
 			# TODO: cookies
-			new_user = User(data["username"], data["email"], data["password"])
+			hashed_password = hashlib.md5(data["password"].encode())
+			new_user = User(data["username"], data["email"], hashed_password.hexdigest())
 			new_user.push()
 			return jsonify({ "success": True }), 200
 		else:
