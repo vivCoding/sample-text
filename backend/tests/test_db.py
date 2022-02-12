3ad026708bff0e479abdfa2590a4fa7d1ba81014
eyang2020 <kenzox2234@gmail.com>
2022-02-12 00:19:08 -0500

    added randomized testing

--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -1,19 +1,33 @@
 from database.user import User
 from runtests import mongodb
+from utils.generate_random import generate_user
 
-user = User(username="bob123", email="bob123@gmail.com", password="bobPassword")
+good_user = generate_user(good=True)
+bad_user = generate_user(good=False)
 
 def test_push_user(mongodb):
-    assert User.push(user), "Push on user failed"
+    assert User.push(good_user), "Push on user failed"
 
 def test_find_user_with_valid_username(mongodb):
-    assert User.find_by_username("bob123") == user, "Could not find user by username"
+    assert User.find_by_username(good_user.username) == good_user, "Could not find user by username"
 
 def test_find_user_with_invalid_username(mongodb):
-    assert User.find_by_username("random123") is None, "Found nonexistent user by username"
+    assert User.find_by_username(bad_user.username)) is None, "Found nonexistent user by username"
 
 def test_find_user_with_valid_email(mongodb):
-    assert User.find_by_email("bob123@gmail.com") == user, "Could not find user by email"
+    assert User.find_by_email(good_user.email) == good_user, "Could not find user by email"
 
 def test_find_user_with_invalid_email(mongodb):
-    assert User.find_by_email("random123@gmail.com") is None, "Found nonexistent user by email"
+    assert User.find_by_email(bad_user.email) is None, "Found nonexistent user by email"
+
+def test_update_user_username(mongodb):
+    pass
+
+def test_update_user_email(mongodb):
+    pass
+
+def test_update_user_password(mongodb):
+    pass
+
+def test_delete_user(mongodb):
+    pass
---
+++ backend/utils/generate_random.py
@@ -0,0 +1,45 @@
+import random
+from database.user import User
+
+good_username_characters = "-_."
+bad_username_characters = """~`!@#$%^&*()+={[}\]|\:;'"<,>?\/"""
+
+def generate_good_username():
+    sze = random.randint(1, 20)
+    ret = ""
+    for i in range(sze):
+        c = random.randrange(62 + len(good_username_characters))
+        if c <= 25:
+            ret += chr(ord('a')+c)
+        elif c <= 51:
+            ret += chr(ord('A')+c-26)
+        elif c <= 61:
+            ret += chr(ord('0')+c-52)
+        else:
+            ret += good_username_characters[c-62]
+    return ret;
+
+def generate_bad_username():
+    sze = random.randint(0, 50)
+    ret = ""
+    for i in range(sze):
+        c = random.randrange(62 + len(good_username_characters) + len(bad_username_characters))
+        if c <= 25:
+            ret += chr(ord('a')+c)
+        elif c <= 51:
+            ret += chr(ord('A')+c-26)
+        elif c <= 61:
+            ret += chr(ord('0')+c-52)
+        elif c <= 64:
+            ret += good_username_characters[c-62]
+        else:
+            ret += bad_username_characters[c-65]
+    if sze > 0:
+        ret += bad_username_characters[random.randrange(0, len(bad_username_characters))]
+    return ret
+
+def generate_user(good):
+    username = generate_bad_username() if not good else generate_good_username
+    email = f"{username}@gmail.com"
+    password = "password"
+    return User(username, email, password)
