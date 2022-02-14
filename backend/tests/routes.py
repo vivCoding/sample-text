d9fe5940b714407866ca9f9c443af3aa0ff83ae5
aorak <aldiyraorak@gmail.com>
2022-02-13 20:05:59 -0500

    Tests

--- backend/tests/routes.py
+++ backend/tests/routes.py
@@ -1,11 +1,13 @@
 from urllib import response
 from flask import Flask
 from flask import Blueprint, jsonify, request
+import pytest
+
 '''
 Module not Found error here when
 pytest tests/routes.py do not know how to link app.py
 '''
-import app
+
 
 def test_index():
     client = app.test_client()
