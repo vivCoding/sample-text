d247b095c9ab274eddce7a8ccb772115ad652235
aorak <aldiyraorak@gmail.com>
2022-02-14 19:31:12 -0500

    Some more route tests

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,6 +1,7 @@
 from cgi import test
+from flask import Flask, jsonify
+from tkinter.tix import Tree
 from urllib import response
-
 from click import password_option
 from runtests import test_client
 from flask.testing import FlaskClient
@@ -17,8 +18,37 @@ def test_user_creation(test_client):
         "email": "aorak@purdue.edu",
         "password": "CS307iscool!"
         })
-    data = response.get_data(as_text=True)
+    data = response.json
     assert response.status_code == 200
-    assert data["success"] == "true"
+    assert data["success"] == True
 
+def test_bad_email(test_client):
+    response = test_client.post("/api/user/createaccount", json={
+        "username": "zhop",
+        "email": "aorakatpurdue.edu",
+        "password": "CS307iscool!"
+        })
+    data = response.json
+    assert response.status_code == 200
+    assert data["success"] == False and (data["error"] == 3), "Bad email assertion failed"
+
+def test_bad_password(test_client):
+    response = test_client.post("/api/user/createaccount", json={
+        "username": "pass",
+        "email": "pass@purdue.edu",
+        "password": "12345678910111213141516171819202122"
+        })
+    data = response.json
+    assert response.status_code == 200
+    assert data["success"] == False and (data["error"] == 4), "Bad Password assertion failed"
+
+def test_exists(test_client):
     
+    response = test_client.post("/api/user/createaccount", json={
+        "username": "pass",
+        "email": "pass@purdue.edu",
+        "password": "12345678910111213141516171819202122"
+        })
+    data = response.json
+    assert response.status_code == 200
+    assert data["success"] == False and (data["error"] == 4), "Bad Password assertion failed"
