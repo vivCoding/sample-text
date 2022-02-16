233a4cd78aa037d17db7cbe931d01610669c2075
aorak <aorak@purdue.edu>
2022-02-16 18:14:54 -0500

    Making assertions more dynamic

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -22,8 +22,8 @@ def test_user_creation(test_client):
     data = response.json
     if User.find_by_email(user.email):
         User.delete_by_email(user.email)
-    assert response.status_code == 200
-    assert data["success"] == True, "User creation test failed"
+    assert response.status_code == 200, "User Creation Test: Status Code " + response.status_code
+    assert data["success"] == True, "User Creation Test: user creation was not successful"
     
 def test_bad_email(test_client):
     user = generate_random.generate_user(True)
@@ -36,8 +36,9 @@ def test_bad_email(test_client):
     data = response.json
     if User.find_by_email(user.email):
         User.delete_by_email(user.email)
-    assert response.status_code == 200
-    assert data["success"] == False and (data["error"] == 3), "Bad Email Assertion Failed"
+    assert response.status_code == 200, "Bad Email Test: Status Code " + response.status_code
+    assert data["success"] == False, "Bad Email Test: No Connection"
+    assert (data["error"] == 3), "Bad Email Test: creation test error " + data["error"]
 
 
 def test_bad_password(test_client):
@@ -50,5 +51,6 @@ def test_bad_password(test_client):
     data = response.json
     if User.find_by_email(user.email):
         User.delete_by_email(user.email)
-    assert response.status_code == 200
-    assert data["success"] == False and (data["error"] == 4), "Bad Password Assertion Failed"
+    assert response.status_code == 200, "Bad Password Test: Status Code " + response.status_code
+    assert data["success"] == False, "Bad Password Test: No Connection"
+    assert (data["error"] == 4), "Bad Password Test: creation test error " + data["error"]
