fec1477d9c20e7c39fbdf7191e16d2337232aed2
aorak <aorak@purdue.edu>
2022-02-19 21:02:58 -0500

    Making tests more informative

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -2,6 +2,14 @@ from flask import session
 from runtests import test_client
 from database.user import User
 from utils import generate_random
+import test_check_creation_fields
+
+goodusername = test_check_creation_fields.generate_good_username()
+goodemail = test_check_creation_fields.generate_good_email()
+badusername = test_check_creation_fields.generate_bad_username
+bademail = "12345"
+badpass = "123"
+goodpass = "12345678"
 
 def test_index(test_client):
     response = test_client.get("/")
@@ -9,10 +17,6 @@ def test_index(test_client):
     assert test_client.get("/api/user/").status_code == 200, "Could not get user index route!"
 
 def test_user_creation(test_client):
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d9302c0 (Merging with dev branch)
     try:
         user = generate_random.generate_user(True)
         response = test_client.post("/api/user/createaccount", json={
@@ -21,6 +25,7 @@ def test_user_creation(test_client):
             "password": user.password
             })
         data = response.json
+
         assert response.status_code == 200
         assert data["success"] == True, "User creation test failed"
         assert user.username in session, "User session not added"
@@ -29,49 +34,29 @@ def test_user_creation(test_client):
             User.delete_by_email(user.email)
         if user.username in session:
             session.pop(user.username)
-<<<<<<< HEAD
-=======
+    
+def test_bad_email(test_client):
     response = test_client.post("/api/user/createaccount", json={
         "username": goodusername,
-        "email": goodemail,
+        "email": bademail,
         "password": goodpass
         })
+    
     data = response.json
     if User.find_by_email(goodemail):
         User.delete_by_email(goodemail)
-    assert response.status_code == 200, "User Creation Test: Status Code " + response.status_code
-    assert data["success"] == True, "User Creation Test: user was not created"
-    
->>>>>>> 909637a (Made tests more consistent)
-=======
->>>>>>> d9302c0 (Merging with dev branch)
-    
-def test_bad_email(test_client):
-    user = generate_random.generate_user(True)
-    response = test_client.post("/api/user/createaccount", json={
-        "username": user.username,
-        "email": f"{user.username}atgmail.com",
-        "password": user.password
-        })
-    
-    data = response.json
-    if User.find_by_email(user.email):
-        User.delete_by_email(user.email)
-    assert response.status_code == 200
-    assert data["success"] == False and (data["error"] == 3), "Bad Email Assertion Failed"
+    assert response.status_code == 200, "Bad Email Test: Status Code " + response.status_code
+    assert data["success"] == False, "Bad Email Test: No Connection"
+    assert (data["error"] == 3), "Bad Email Test: creation test error "
+
 
 def test_bad_password(test_client):
-    user = generate_random.generate_user(True)
     response = test_client.post("/api/user/createaccount", json={
-        "username": user.username,
-        "email": user.email,
-        "password": "123456789101112131415161718"
+        "username": goodusername,
+        "email": goodemail,
+        "password": badpass
         })
     data = response.json
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d9302c0 (Merging with dev branch)
     if User.find_by_email(user.email):
         User.delete_by_email(user.email)
     assert response.status_code == 200
@@ -112,15 +97,4 @@ def test_login_existing(test_client):
         if User.find_by_email(user.email):
             User.delete_by_email(user.email)
         if user.username in session:
-<<<<<<< HEAD
             session.pop(user.username)
-=======
-    if User.find_by_email(goodemail):
-        User.delete_by_email(goodemail)
-    assert response.status_code == 200, "Bad Password Test: Status Code " + response.status_code
-    assert data["success"] == False, "Bad Password Test: No Connection"
-    assert (data["error"] == 4), "Bad Password Test: creation test error " + data["error"]
->>>>>>> 909637a (Made tests more consistent)
-=======
-            session.pop(user.username)
->>>>>>> d9302c0 (Merging with dev branch)
