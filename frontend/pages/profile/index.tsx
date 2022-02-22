5980e3c31d1d663ead4d65d7c9e99266529179f2
vivCoding <vivCoding@gmail.com>
2022-02-22 15:37:57 -0500

    profile page

--- frontend/pages/profile/index.tsx
+++ frontend/pages/profile/index.tsx
@@ -6,21 +6,47 @@ import { useSelector } from 'react-redux';
 import Helmet from '../../src/components/common/Helmet';
 import UserNavbar from '../../src/components/navbar/user';
 import { ReduxStoreType } from '../../src/types/redux';
+import ProfileAvatar from '../../src/components/common/ProfileAvatar';
+import Link from '../../src/components/common/Link';
 
 const ProfilePage: NextPage = () => {
-    const { username } = useSelector((state: ReduxStoreType) => state.user)
+    const {
+        username, name, bio, pfp,
+    } = useSelector((state: ReduxStoreType) => state.user)
 
     return (
         <Box>
             <Helmet title="Sample Text" />
             <UserNavbar />
             <Container sx={{ mt: 5 }}>
-                <Grid>
-                    <Grid item xs={12}>
-                        <Typography variant="h3">{username}</Typography>
+                {/* <Typography variant="h4" fontWeight="light" sx={{ mb: 3 }}>{username}</Typography> */}
+                <Grid spacing={2} container>
+                    <Grid item xs={3}>
+                        <ProfileAvatar size={200} picture64={pfp} />
                     </Grid>
+                    <Grid item xs={7} container>
+                        <Grid item xs={12}>
+                            <Typography variant="h4">{name}</Typography>
+                            <Typography variant="body1" fontWeight="light">
+                                u/
+                                {username}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="body1" fontWeight="normal">{bio}</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button variant="contained" component={Link} href="/settings">Edit Profile</Button>
+                        </Grid>
+                    </Grid>
+                    {/* <Grid item xs={2}>
+                        <Stack>
+                            <Typography variant="h6">12 Posts</Typography>
+                            <Typography variant="h6">324 Followers</Typography>
+                            <Typography variant="h6">324 Following</Typography>
+                        </Stack>
+                    </Grid> */}
                 </Grid>
-
             </Container>
         </Box>
     )
