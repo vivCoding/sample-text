e16da264c1efa30e4f1dcff1687660811f92dc51
Aldiyar Orak <aldiyarorak@pal-nat186-145-108.itap.purdue.edu>
2022-02-22 22:12:46 -0500

    Problems with merging

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -35,13 +35,8 @@ def test_bad_email(test_client):
         })
     
     data = response.json
-<<<<<<< HEAD
     if User.find_by_email(user.email):
         User.delete_by_email(user.email)
-=======
-    if User.find_by_email(bademail):
-        User.delete_by_email(bademail)
->>>>>>> 798cb98 (Making tests more informative)
     assert response.status_code == 200, "Bad Email Test: Status Code " + response.status_code
     assert data["success"] == False, "Bad Email Test: No Connection"
     assert (data["error"] == 3), "Bad Email Test: creation test error " + data["error"] + "\n Problem with email:" + bademail
