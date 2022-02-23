0fc0408f24b9a41c190832dbbfeb944fe149f685
Aldiyar Orak <aldiyarorak@pal-nat186-145-108.itap.purdue.edu>
2022-02-22 22:17:11 -0500

    Made tests more consistent

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -2,6 +2,14 @@ from flask import session
 from runtests import test_client
 from database.user import User
 from utils import generate_random
+import test_check_creation_fields
+
+goodusername = test_check_creation_fields.generate_good_username()
+goodemail = test_check_creation_fields.generate_good_email()
+badusername = test_check_creation_fields.generate_bad_username
+bademail = "12345"
+badpass = "123"
+goodpass = "12345678"
 
 def test_index(test_client):
     response = test_client.get("/")
@@ -9,6 +17,7 @@ def test_index(test_client):
     assert test_client.get("/api/user/").status_code == 200, "Could not get user index route!"
 
 def test_user_creation(test_client):
+<<<<<<< HEAD
     try:
         user = generate_random.generate_user(True)
         response = test_client.post("/api/user/createaccount", json={
@@ -25,32 +34,48 @@ def test_user_creation(test_client):
             User.delete_by_email(user.email)
         if user.username in session:
             session.pop(user.username)
+=======
+    response = test_client.post("/api/user/createaccount", json={
+        "username": goodusername,
+        "email": goodemail,
+        "password": goodpass
+        })
+    data = response.json
+    if User.find_by_email(goodemail):
+        User.delete_by_email(goodemail)
+    assert response.status_code == 200, "User Creation Test: Status Code " + response.status_code
+    assert data["success"] == True, "User Creation Test: "
+    
+>>>>>>> 909637a (Made tests more consistent)
     
 def test_bad_email(test_client):
-    user = generate_random.generate_user(True)
     response = test_client.post("/api/user/createaccount", json={
-        "username": user.username,
-        "email": f"{user.username}atgmail.com",
-        "password": user.password
+        "username": goodusername,
+        "email": bademail,
+        "password": goodpass
         })
     
     data = response.json
-    if User.find_by_email(user.email):
-        User.delete_by_email(user.email)
+    if User.find_by_email(goodemail):
+        User.delete_by_email(goodemail)
     assert response.status_code == 200, "Bad Email Test: Status Code " + response.status_code
     assert data["success"] == False, "Bad Email Test: No Connection"
+<<<<<<< HEAD
     assert (data["error"] == 3), "Bad Email Test: creation test error " + data["error"] + "\n Problem with email:" + bademail
 
+=======
+    assert (data["error"] == 3), "Bad Email Test: creation test error "
+>>>>>>> 909637a (Made tests more consistent)
 
 
 def test_bad_password(test_client):
-    user = generate_random.generate_user(True)
     response = test_client.post("/api/user/createaccount", json={
-        "username": user.username,
-        "email": user.email,
-        "password": "123456789101112131415161718"
+        "username": goodusername,
+        "email": goodemail,
+        "password": badpass
         })
     data = response.json
+<<<<<<< HEAD
     if User.find_by_email(user.email):
         User.delete_by_email(user.email)
     assert response.status_code == 200
@@ -92,3 +117,10 @@ def test_login_existing(test_client):
             User.delete_by_email(user.email)
         if user.username in session:
             session.pop(user.username)
+=======
+    if User.find_by_email(goodemail):
+        User.delete_by_email(goodemail)
+    assert response.status_code == 200, "Bad Password Test: Status Code " + response.status_code
+    assert data["success"] == False, "Bad Password Test: No Connection"
+    assert (data["error"] == 4), "Bad Password Test: creation test error " + data["error"]
+>>>>>>> 909637a (Made tests more consistent)
