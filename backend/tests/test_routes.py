c6cdee2da4865b809c83175709d906b2f3f13c5f
Aldiyar Orak <aldiyarorak@pal-nat186-154-134.itap.purdue.edu>
2022-02-23 18:29:31 -0500

    A lot of tests on profile editing

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,3 +1,8 @@
+import email
+from hashlib import new
+import profile
+from re import A
+from unicodedata import name
 from flask import session
 from runtests import test_client
 from database.user import User
@@ -20,7 +25,7 @@ def test_user_creation(test_client):
 
         assert response.status_code == 200
         assert data["success"] == True, "User creation test failed"
-        assert user.username in session, "User session not added"
+        assert session["username"] == user.username, "User session not added"
     finally:
         if User.find_by_email(user.email):
             User.delete_by_email(user.email)
@@ -85,6 +90,171 @@ def test_login_existing(test_client):
         assert response.status_code == 200 or response.status_code == 302
         assert data["success"] == True, "Login to existing user test failed"
         assert user.username in session, "Username not added to session"
+    finally:
+        if User.find_by_email(user.email):
+            User.delete_by_email(user.email)
+        if user.username in session:
+            session.pop(user.username)
+
+def test_delete_account(test_client):
+    try:
+        user = generate_random.generate_user(True)
+        response = test_client.post("/api/user/createaccount", json={
+            "username": user.username,
+            "email": user.email,
+            "password": user.password,
+        })
+
+        assert response.status_code == 200, "Creating user in update profile test failed"
+
+        response = test_client.post("/api/user/delete", json={
+            "username": user.username,
+            "email" : user.email,
+            "password": user.password
+        })
+        data = response.json
+        
+        assert response.status_code == 200
+        assert data["success"] == True, "Delete user profile test failed"
+        deleted_user = User.find_by_email(user.email)
+        assert deleted_user is None, "User deletion failed"
+    finally:
+        if User.find_by_email(user.email):
+            User.delete_by_email(user.email)
+        if user.username in session:
+            session.pop(user.username)
+
+def test_edit_profile_username(test_client):
+    try:
+        user = generate_random.generate_user(True)
+        response = test_client.post("/api/user/editprofile", json={
+            "username": user.username,
+            "email": user.email,
+            "password": user.password,
+        })
+
+        assert response.status_code == 200, "Creating user in update profile test failed"
+        
+        new_username = generate_random.generate_user(True).username
+
+        response = test_client.post("/api/user/updateprofile", json={
+            "username": new_username,
+            "email" : user.email,
+            "password": user.password
+        })
+
+        data = response.json
+        dbuser = User.find_by_username(new_username)
+        
+        
+        assert response.status_code == 200
+        assert data["success"] == True, "Updating username test failed"
+        assert session['username'] == new_username, "Session username mismatch"
+        assert dbuser.username == new_username and dbuser.password == user.password, "Mismatch in db and new user"
+
+
+    finally:
+        if User.find_by_email(user.email):
+            User.delete_by_email(user.email)
+        if user.username in session:
+            session.pop(user.username)
+
+def test_edit_profile_password(test_client):
+    try:
+        user = generate_random.generate_user(True)
+        response = test_client.post("/api/user/createaccount", json={
+            "username": user.username,
+            "email": user.email,
+            "password": user.password,
+        })
+
+        assert response.status_code == 200, "Creating user in update profile test failed"
+        
+        new_user_pass = generate_random.generate_user(True).password
+
+        response = test_client.post("/api/user/editprofile", json={
+            "username": user.username,
+            "email" : user.email,
+            "password": new_user_pass
+        })
+
+        data = response.json
+        dbuser = User.find_by_username(user.username)
+
+        
+        assert response.status_code == 200
+        assert data["success"] == True, "Updating password test failed"
+        assert dbuser.username == user.username and dbuser.password == new_user_pass, "Mismatch in db and new user"
+
+    finally:
+        if User.find_by_email(user.email):
+            User.delete_by_email(user.email)
+        if user.username in session:
+            session.pop(user.username)
+
+def test_edit_profile_email(test_client):
+    try:
+        user = generate_random.generate_user(True)
+        response = test_client.post("/api/user/createaccount", json={
+            "username": user.username,
+            "email": user.email,
+            "password": user.password,
+        })
+
+        assert response.status_code == 200, "Creating user in update profile test failed"
+        
+        new_user_email = generate_random.generate_user(True).email
+
+        response = test_client.post("/api/user/editprofile", json={
+            "username": user.username,
+            "email" : new_user_email,
+            "password": user.password
+        })
+
+        data = response.json
+        dbuser = User.find_by_email(new_user_email)
+
+        
+        assert response.status_code == 200, "Connection was not established"
+        assert data["success"] == True, "Updating email test failed"
+        assert dbuser is not None, "Database info mismatch"
+
+    finally:
+        if User.find_by_email(user.email):
+            User.delete_by_email(user.email)
+        if User.find_by_email(new_user_email):
+            User.delete_by_email(new_user_email)
+        if user.username in session:
+            session.pop(user.username)
+
+def test_edit_profile_credentials(test_client):
+    try:
+        user = generate_random.generate_user(True)
+        response = test_client.post("/api/user/createaccount", json={
+            "username": user.username,
+            "email": user.email,
+            "password": user.password,
+        })
+
+        assert response.status_code == 200, "Creating user in update profile test failed"
+
+        response = test_client.post("/api/user/editprofile", json={
+            "username": user.username,
+            "email" : user.email,
+            "password": user.password,
+            "name": "Aldiyar",
+            "bio": "cool guy",
+            "profile_img": "12345678"
+        })
+        data = response.json
+
+        
+        assert response.status_code == 200 or response.status_code == 302
+        assert data["success"] == True, "Updating user profile test failed"
+
+        new_user = User.find_by_email(user.email)
+        assert new_user is not None, "User does not exist"
+        assert new_user.name == "Aldiyar" and new_user.bio == "cool guy" and new_user.profile_img == "12345678", "Bad credentials returned in update profile test"
     finally:
         if User.find_by_email(user.email):
             User.delete_by_email(user.email)
