49564f50c041acb2d172ae54c5ce75c252d0e6b5
Aldiyar Orak <aldiyarorak@pal-nat186-145-108.itap.purdue.edu>
2022-02-22 21:26:59 -0500

    Merging with dev

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -2,14 +2,6 @@ from flask import session
 from runtests import test_client
 from database.user import User
 from utils import generate_random
-import test_check_creation_fields
-
-goodusername = test_check_creation_fields.generate_good_username()
-goodemail = test_check_creation_fields.generate_good_email()
-badusername = test_check_creation_fields.generate_bad_username
-bademail = "12345"
-badpass = "123"
-goodpass = "12345678"
 
 def test_index(test_client):
     response = test_client.get("/")
@@ -25,7 +17,6 @@ def test_user_creation(test_client):
             "password": user.password
             })
         data = response.json
-
         assert response.status_code == 200
         assert data["success"] == True, "User creation test failed"
         assert user.username in session, "User session not added"
@@ -36,25 +27,25 @@ def test_user_creation(test_client):
             session.pop(user.username)
     
 def test_bad_email(test_client):
+    user = generate_random.generate_user(True)
     response = test_client.post("/api/user/createaccount", json={
-        "username": goodusername,
-        "email": bademail,
-        "password": goodpass
+        "username": user.username,
+        "email": f"{user.username}atgmail.com",
+        "password": user.password
         })
     
     data = response.json
-    if User.find_by_email(goodemail):
-        User.delete_by_email(goodemail)
-    assert response.status_code == 200, "Bad Email Test: Status Code " + response.status_code
-    assert data["success"] == False, "Bad Email Test: No Connection"
-    assert (data["error"] == 3), "Bad Email Test: creation test error "
-
+    if User.find_by_email(user.email):
+        User.delete_by_email(user.email)
+    assert response.status_code == 200
+    assert data["success"] == False and (data["error"] == 3), "Bad Email Assertion Failed"
 
 def test_bad_password(test_client):
+    user = generate_random.generate_user(True)
     response = test_client.post("/api/user/createaccount", json={
-        "username": goodusername,
-        "email": goodemail,
-        "password": badpass
+        "username": user.username,
+        "email": user.email,
+        "password": "123456789101112131415161718"
         })
     data = response.json
     if User.find_by_email(user.email):
