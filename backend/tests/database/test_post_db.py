9fe708104d3ebb9c6318c8870afb7a8f452ab636
devinqu <devinqu02@gmail.com>
2022-03-24 17:41:34 -0900

    separate tests for liking and unliking posts

--- backend/tests/database/test_post_db.py
+++ backend/tests/database/test_post_db.py
@@ -6,25 +6,31 @@ from utils.generate_random import generate_user
 from database.topic import Topic
 
 def test_post(mongodb):
-    user = generate_user(good=True)
-    if User.find_by_username(user.email) is None:
+    user = generate_user(True)
+    if User.find_by_username(user.username) is None:
         user.push()
+
     # test post creation
     post = Post(title="My first post", topic="Games", author_id=user.user_id)
-    post.push()
-    assert post.post_id == Post.find(post.post_id).post_id, "Could not find post"
+    post.push() 
+    assert Post.find(post.post_id).post_id is not None, "Could not find post"
     assert post.post_id in User.find_by_username(user.username).posts, "Post was not added to the user"
-    # test update likes
-    likeCount = len(post.likes)
-    post.like("emiru")
-    post.like("emiru")
-    post.like("tyler1")
-    assert len(Post.find(post.post_id).likes) == likeCount + 2, "Likes was not updated correctly"
+
+    # test like post
+    post.like(user.user_id)
+    assert len(Post.find(post.post_id).likes) == 1, "Likes not updated correctly"
+    assert user.user_id in Post.find(post.post_id).likes, "user_id not added to list of users who liked"
+
+    # test unlike post
+    post.unlike(user.user_id)
+    assert len(Post.find(post.post_id).likes) == 0, "Likes not updated correctly"
+    assert user.user_id not in Post.find(post.post_id).likes, "user_id not removed from list of users who liked"
+
     # test add comment
     pair = ["xqcow1", "epic post dude! wow!"]
     post.add_comment(user_id=pair[0], comment=pair[1])
     assert pair in post.comments, "Comment was not added"
-    # test post deletion111
+    # test post deletion
     Post.delete(post.post_id)
     assert Post.find(post.post_id) is None, "Post was not deleted"
     User.delete_by_username(user.username)
