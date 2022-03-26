26930ebe9ec50e97d09cf4952c02332b67c36821
devinqu <devinqu02@gmail.com>
2022-03-26 16:33:17 -0400

    add tests to check the like lists for when the user or post get deleted

--- backend/tests/database/test_post_db.py
+++ backend/tests/database/test_post_db.py
@@ -7,24 +7,38 @@ from database.topic import Topic
 
 def test_post(mongodb):
     user = generate_user(True)
+    user2 = generate_user(True)     # Delete post first
+    user3 = generate_user(True)     # Delete user first
     if User.find_by_username(user.username) is None:
         user.push()
+    if User.find_by_username(user2.username) is None:
+        user2.push()
+    if User.find_by_username(user3.username) is None:
+        user3.push()
 
     # test post creation
     post = Post(title="My first post", topic="Games", author_id=user.user_id)
-    post.push() 
+    post2 = Post(title="second post", topic="Games", author_id=user2.user_id)
+    post3 = Post(title="third post", topic="Games", author_id=user3.user_id)
+    post.push()
+    post2.push()
+    post3.push()
     assert Post.find(post.post_id).post_id is not None, "Could not find post"
     assert post.post_id in User.find_by_username(user.username).posts, "Post was not added to the user"
 
     # test like post
     post.like(user.user_id)
+    post2.like(user2.user_id)
+    post3.like(user3.user_id)
     assert len(Post.find(post.post_id).likes) == 1, "Likes not updated correctly"
     assert user.user_id in Post.find(post.post_id).likes, "user_id not added to list of users who liked"
+    assert post.post_id in User.find_by_username(user.username).liked_posts, "post_id not added to user's list of liked posts"
 
     # test unlike post
     post.unlike(user.user_id)
     assert len(Post.find(post.post_id).likes) == 0, "Likes not updated correctly"
     assert user.user_id not in Post.find(post.post_id).likes, "user_id not removed from list of users who liked"
+    assert post.post_id not in User.find_by_username(user.username).liked_posts, "post_id not removed from user's list of liked posts"
 
     # test add comment
     pair = ["xqcow1", "epic post dude! wow!"]
@@ -34,3 +48,17 @@ def test_post(mongodb):
     Post.delete(post.post_id)
     assert Post.find(post.post_id) is None, "Post was not deleted"
     User.delete_by_username(user.username)
+
+    # test that deleting post removes like from user
+    Post.delete(post2.post_id)
+    user2 = User.find_by_username(user2.username)
+    assert post2.post_id not in user2.liked_posts, "post_id not removed from user's list of liked posts"
+
+    # test that deleting user removes like from post
+    user3 = User.find_by_username(user3.username)
+    User.delete_by_username(user3.username)
+    post3 = Post.find(post3.post_id)
+    assert user3.user_id not in post3.likes and len(post3.likes) == 0, "user_id not removed from post's likes after deleting user"
+
+    Post.delete(post3.post_id)
+    User.delete_by_username(user2.username)
