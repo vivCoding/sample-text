52a4baafea0e3c3128183e240fa3355e6fdb276e
devinqu <devinqu02@gmail.com>
2022-03-26 20:43:11 -0400

    clean up test, check the post_id gets added correctly

--- backend/tests/routes/test_post_route.py
+++ backend/tests/routes/test_post_route.py
@@ -205,51 +205,61 @@ def test_unlike_post(test_client):
         test_client.cookie_jar.clear()
 
 def test_comment_on_post(test_client):
-    # simulate logged in user
-    user = generate_user(True)
-    response = test_client.post("/api/user/createaccount", json={
-        "username": user.username,
-        "email": user.email,
-        "password": user.password,
-    })
-    assert response.status_code == 200, "Bad create account reponse " + str(response.status_code)
-    # user creates a post
-    user_data = response.json
-    post = Post(title="My second post", topic="Games", author_id=user_data["data"]["userId"])
-    response = test_client.post("/api/post/createpost", json={
-        "title": post.title,
-        "topic": post.topic,
-        "author_id": post.author_id,
-        "img": post.img,
-        "caption": post.caption,
-        "anonymous": post.anonymous,
-        "likes": post.likes,
-        "comments": post.comments,
-        "date": post.date,
-        "post_id": post.post_id
+    try:
+        # simulate logged in user
+        user = generate_user(True)
+        response = test_client.post("/api/user/createaccount", json={
+            "username": user.username,
+            "email": user.email,
+            "password": user.password,
         })
-    assert response.status_code == 200, "Bad response, got " + str(response.status_code)
-    data = response.json
-    post.post_id = data['data']['post_id']
-    assert data["success"] == True, f"Post creation test failed for: {str(post.to_dict())}, error: {data.get('error', None)}"
-    assert Post.find(post.post_id) is not None, "Post was not found in database"
-    # valid comment on a post
-    comment = "This is a valid comment"
-    response = test_client.post("/api/post/commentonpost", json={
-        "post_id": post.post_id,
-        "comment": comment
-        })
-    data = response.json
-    assert response.status_code == 200, "Bad response, got " + str(response.status_code)
-    assert data["success"] == True, f"Comment on post test failed for: {comment}, error: {data.get('errorMessage', None)}"
-    assert [user_data["data"]["userId"], comment] in data["data"]["comments"], "Comment was not added correctly"
-    # invalid comment on a post
-    comment = ""
-    response = test_client.post("/api/post/commentonpost", json={
-        "post_id": post.post_id,
-        "comment": comment
-        })
-    data = response.json
-    assert response.status_code == 200, "Bad response, got " + str(response.status_code)
-    assert data["success"] == False, f"Comment on post test succeeded for: {comment}, error: {data.get('errorMessage', None)}"
-    test_client.cookie_jar.clear()
+        assert response.status_code == 200, "Bad create account reponse " + str(response.status_code)
+
+        # user creates a post
+        user_data = response.json
+        post = Post(title="My second post", topic="Games", author_id=user_data["data"]["userId"])
+        response = test_client.post("/api/post/createpost", json={
+            "title": post.title,
+            "topic": post.topic,
+            "author_id": post.author_id,
+            "img": post.img,
+            "caption": post.caption,
+            "anonymous": post.anonymous,
+            "likes": post.likes,
+            "comments": post.comments,
+            "date": post.date,
+            "post_id": post.post_id
+            })
+        assert response.status_code == 200, "Bad response, got " + str(response.status_code)
+        data = response.json
+        post.post_id = data['data']['post_id']
+        assert data["success"] == True, f"Post creation test failed for: {str(post.to_dict())}, error: {data.get('error', None)}"
+        assert Post.find(post.post_id) is not None, "Post was not found in database"
+
+        # valid comment on a post
+        comment = "This is a valid comment"
+        response = test_client.post("/api/post/commentonpost", json={
+            "post_id": post.post_id,
+            "comment": comment
+            })
+        data = response.json
+        assert response.status_code == 200, "Bad response, got " + str(response.status_code)
+        assert data["success"] == True, f"Comment on post test failed for: {comment}, error: {data.get('errorMessage', None)}"
+        assert [user_data["data"]["userId"], comment] in data["data"]["comments"], "Comment was not added correctly"
+        assert post.post_id in User.find_by_email(user.email).comments, "post_id not added to user's list of comments"
+
+        # invalid comment on a post
+        comment = ""
+        response = test_client.post("/api/post/commentonpost", json={
+            "post_id": post.post_id,
+            "comment": comment
+            })
+        data = response.json
+        assert response.status_code == 200, "Bad response, got " + str(response.status_code)
+        assert data["success"] == False, f"Comment on post test succeeded for: {comment}, error: {data.get('errorMessage', None)}"
+    finally:
+        if User.find_by_email(user.email):
+            User.delete_by_email(user.email)
+        if Post.find(post.post_id):
+            Post.delete(post.post_id)
+        test_client.cookie_jar.clear()
