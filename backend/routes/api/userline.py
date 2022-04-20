402679d050e24e64d5aaf288ba454af02a682d09
eyang2020 <kenzox2234@gmail.com>
2022-04-19 22:34:06 -0400

    add userline

---
+++ backend/routes/api/userline.py
@@ -0,0 +1,54 @@
+from typing import OrderedDict
+from flask import Blueprint, jsonify, request, session
+from utils.encrypt import encrypt
+from utils.validate_fields import check_post_fields, check_comment
+from database.user import User
+from database.post import Post
+from database.topic import Topic
+from datetime import datetime
+
+userline_blueprint = Blueprint("userline", __name__)
+
+@userline_blueprint.route('/')
+def index():
+	return jsonify("api userline index")
+
+@userline_blueprint.route('/generateuserline', methods=["POST"])
+def generate_userline():
+	# do not proceed if user is not logged in
+	# if they are logged in, they should have their user_id in their session cookie
+	user_id = session.get('user_id', None)
+	if user_id is None:
+		return jsonify({ "success": False }), 401
+	try:
+		user = User.find_by_id(user_id)
+		if user is not None:
+			userline = []
+			# generate userline
+			# interactions: like, comment, save, dislike, love
+			# get posts user has liked
+			for post_id in user.liked_posts:
+				date = Post.find(post_id).date
+				userline.append([post_id, "Liked", date])
+			# get posts user has commented on
+			for post_id in user.comments:
+				date = Post.find(post_id).date
+				userline.append([post_id, "Commented", date])
+			# get user's saved posts
+			for post_id in user.saved_posts:
+				date = Post.find(post_id).date
+				userline.append([post_id, "Saved", date])
+			# TODO: get posts user has disliked
+
+			# TODO: get posts user has loved
+
+			# sort mp by post creation time
+			userline = sorted(userline, key = lambda x:datetime.strptime(x[2], '%Y/%m/%d, %H:%M:%S'), reverse=True)
+			#print(userline)
+			#assert 1 == 2
+			return jsonify({ "success": True, "data": userline }), 200
+		else:
+			return jsonify({ "success": False }), 405
+	except Exception as e:
+		print (e)
+		return jsonify({"success": False }), 500
---
+++ backend/tests/routes/test_userline_route.py
@@ -0,0 +1,85 @@
+import tempfile
+from time import time
+from flask import session
+from runtests import test_client
+from database.user import User
+from database.post import Post
+from database.topic import Topic
+from utils.generate_random import generate_user, generate_good_topic
+from datetime import datetime
+
+def test_generate_userline(test_client):
+    try:   
+        # generate main user
+        user1 = generate_user(True)
+        response = test_client.post("/api/user/createaccount", json={
+            "username": user1.username,
+            "email": user1.email,
+            "password": user1.password
+            })
+        data = response.json
+        assert response.status_code == 200, "Bad response, got " + str(response.status_code)
+        assert data["success"] == True, f"User creation test failed for: {str(user1.to_dict())}, error: {data.get('error', None)}"
+        assert session.get("user_id", None) is not None, "User session not added for: " + user1.username
+        user1.user_id = session.get("user_id", None)
+        # generate second user
+        user2 = generate_user(True)
+        user2.push()
+        # second user creates a post
+        generated_topic = generate_good_topic()
+        post1 = Post("Check out my post", generated_topic, user2.user_id)
+        post1.push()
+        parent_topic = Topic.find_by_name(post1.topic)
+        if parent_topic is None:
+            parent_topic = Topic(post1.topic, [post1.post_id])
+            parent_topic.push()
+        else:
+            parent_topic.add_post(post1.post_id)
+        user2.add_post(post1.post_id)
+        # second user creates another post
+        generated_topic = generate_good_topic()
+        post2 = Post("Check out my second post", generated_topic, user2.user_id)
+        post2.push()
+        parent_topic = Topic.find_by_name(post2.topic)
+        if parent_topic is None:
+            parent_topic = Topic(post2.topic, [post2.post_id])
+            parent_topic.push()
+        else:
+            parent_topic.add_post(post2.post_id)
+        user2.add_post(post2.post_id)
+        # user1 likes the post
+        post1.like(user1.user_id)
+        post2.like(user1.user_id)
+        # user1 comments on the post
+        post1.add_comment(user1.user_id, "Cool post man!")
+        # user1 saves the post
+        user1.save_post(post1.post_id)
+        # TODO: user1 dislikes the post
+
+        # TODO: user1 loves the post
+        # generate userline
+        response = test_client.post("/api/userline/generateuserline")
+        assert response.status_code == 200, "Bad response, got " + str(response.status_code)
+        data = response.json
+        userline = data['data']
+        print("userline: ", userline)
+        # assert that all post interactions are in the userline
+        assert [post1.post_id, "Liked", post1.date] in userline, "post1 like was not in user1's userline"
+        assert [post2.post_id, "Liked", post2.date] in userline, "post2 like was not in user1's userline"
+        assert [post1.post_id, "Commented", post1.date] in userline, "post1 comment was not in user1's userline"
+        assert [post1.post_id, "Saved", post1.date] in userline, "post1 save was not in user1's userline"
+        # assert that posts are sorted by creation time
+        sortedUserline = sorted(userline, key = lambda x:datetime.strptime(x[2], '%Y/%m/%d, %H:%M:%S'), reverse=True)
+        assert userline == sortedUserline, "userline is not sorted correctly"
+    finally:
+        if User.find_by_email(user1.email):
+            User.delete_by_email(user1.email)
+        if User.find_by_email(user2.email):
+            User.delete_by_email(user2.email)
+        if Post.find(post1.post_id):
+           Post.delete(post1.post_id)
+        if Post.find(post2.post_id):
+           Post.delete(post2.post_id)
+        if "user_id" in session:
+            session.pop("user_id")
+        test_client.cookie_jar.clear()
