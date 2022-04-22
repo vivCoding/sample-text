176a64e60b0490109ffe0ce58d2aa4914e61dfb8
vivCoding <vivCoding@gmail.com>
2022-04-22 15:12:40 -0400

    dark/light mode

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,16 +1,47 @@
 import * as React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
-import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 import { store } from '../src/store'
 
 import 'react-toastify/dist/ReactToastify.min.css'
+
+const darkTheme = createTheme({
+    typography: {
+        fontFamily: 'Roboto',
+    },
+    components: {
+        MuiButtonBase: {
+            defaultProps: {
+                disableRipple: true,
+            },
+        },
+    },
+    palette: {
+        mode: 'dark',
+    },
+});
+
+const lightTheme = createTheme({
+    typography: {
+        fontFamily: 'Roboto',
+    },
+    components: {
+        MuiButtonBase: {
+            defaultProps: {
+                disableRipple: true,
+            },
+        },
+    },
+    palette: {
+        mode: 'light',
+    },
+});
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -27,7 +58,7 @@ const MyApp = (props: MyAppProps): JSX.Element => {
                 <Head>
                     <meta name="viewport" content="initial-scale=1, width=device-width" />
                 </Head>
-                <ThemeProvider theme={theme}>
+                <ThemeProvider theme={darkTheme}>
                     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
                     <CssBaseline />
                     <ToastContainer />
